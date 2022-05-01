68f8b599b83bea843f3e9ce2ee4aa9b5e238febe
X2isme <98345602+X2isme@users.noreply.github.com>
2022-05-01 13:49:07 -0700

    Create Edit_Mic.js

---
+++ MoreInfo/JS Code/Edit_Mic.js
@@ -0,0 +1,20 @@
+navigator.getUserMedia (
+   // constraints
+   {
+      audio: true
+   },
+
+   function(localMediaStream) {
+      var video = document.querySelector('video');
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.onloadedmetadata = function(e) {
+        
+      };
+   },
+
+   function(err) {
+    if(err === "PERMISSION_DENIED") {
+      
+    }
+   }
+);
